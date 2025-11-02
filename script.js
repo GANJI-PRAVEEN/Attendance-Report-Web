b330b0a1e44210eb30e3604c8fe87936519a5d02
ganji <ganjipraveen444@gmail.com>
2025-11-03 02:03:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+
+console.log(window.document);
+function classesNeededToReach75(classesAttended,TotalClasses,classesPerDay,targetDateString){
+  //formula to find the numberOfDays to go to get 75%
+  let totalClassesShouldAtttend = (0.75*(TotalClasses)-classesAttended)/0.25;
+  return totalClassesShouldAtttend;
+
+}
+
+function applyDetails(classesAttended,TotalClasses,classesPerDay,attendancePercentage,targetDateString){
+    let classesAttendSpan  = document.getElementById("fetchClassesAttended");
+    let totalClassesSpan   = document.getElementById("fetchTotalClasses");
+    let classesNeededSpan = document.getElementById("fetchClassesNeeded");
+    let numberOfDaysToGo = document.getElementById("fetchnumberOfDaysToGo");
+    let targetDateSpan = document.getElementById("fetchTargetDate");
+    let remainingWorkingDays = document.getElementById("fetchRemainingWorkingDays");
+    
+    let workingDays = getWorkingDaysUntilToday(targetDateString);
+    remainingWorkingDays.value=''+workingDays;
+    targetDateSpan.value = targetDateString;
+    classesAttendSpan.value=`${classesAttended}`;
+    totalClassesSpan.value=`${TotalClasses}`;
+    let classesMustBeAtttend = classesNeededToReach75(classesAttended,TotalClasses,classesPerDay,targetDateString);
+    classesNeededSpan.value = ''+classesMustBeAtttend;
+    // console.log(ceil(classesMustBeAtttend/classesPerDay));
+    let numberOfDaysToGoValue = Math.ceil(classesMustBeAtttend/classesPerDay);
+    if(numberOfDaysToGoValue>workingDays){
+      document.getElementsByClassName("Input")[3].style.backgroundColor = "#CD5C5C";
+    }
+    else document.getElementsByClassName("Input")[3].style.backgroundColor = "#32CD32";
+    numberOfDaysToGo.value = ''+numberOfDaysToGoValue;
+
+}
+function getWorkingDaysUntilToday(targetDateString){
+    const today = new Date();
+    const targetDate = new Date(targetDateString);
+    let count=0;
+
+    for(let d = new Date(today); d<=targetDate;d.setDate(d.getDate()+1)){
+      const day = d.getDay();
+
+      if(day==0){
+        //sunday
+        continue;
+      }
+
+      if(day==6){
+        const date = d.getDate();
+        if(date>=8 && date<=14){
+          //second saturday of the month
+          continue;
+        }
+      }
+      count++;
+    }
+    return count;
+}
+
+
+
+function calculateAttendance() {
+  let classesAttended = Number(document.getElementById("classesAttended").value);
+  let TotalClasses = Number(document.getElementById("TotalClasses").value);
+  let classesPerDay = Number(document.getElementById("classesPerDay").value);
+  let attendancePercentage = (classesAttended / TotalClasses) * 100;
+  let targetDateString = document.getElementById("targetDate").value;
+
+
+
+  // console.log(document.getElementById("classesAttended").value);
+  let meterCircle = document.getElementsByClassName("meterCircle")[0];
+  meterCircle.style.setProperty('--value',attendancePercentage);
+  currentAttendaceTag.textContent = `Your current Attendance is ${attendancePercentage.toFixed(2)}%`;
+  applyDetails(classesAttended,TotalClasses,classesPerDay,attendancePercentage,targetDateString);
+
+}
+
+let btnSubmit = document.getElementsByClassName("submitBTN")[0];
+btnSubmit.addEventListener('click', calculateAttendance);
+console.log(document.getElementsByClassName("Input"));
+let currentAttendaceTag = document.getElementById("currentAttendace");
+
